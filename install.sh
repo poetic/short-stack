bdbbb4e8986fc370bd2d43f495f994cb391966d0
Matthew Hager <matthew@poeticsystems.com>
2015-02-02 22:53:20 -0600

    Move all dependencies to the install.sh file.

--- install.sh
+++ install.sh
@@ -3,14 +3,32 @@ set -e
 
 echo 'Boostrapping your computer...'
 
-# Xcode Command Line Tools
+# Install xcode command line tools
 if [[ ! -x `xcode-select -p 2>/dev/null` ]]; then
   xcode-select --install
   echo 'Press any key when the installation has completed.'
   read -n 1
 fi
 
-# Clone Short Stack
+# Install homebrew
+if [[ ! -x /usr/local/bin/brew ]]; then
+  echo 'Installing homebrew...'
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Install pip
+if [[ ! -x `which pip` ]]; then
+  echo 'Installing pip...'
+  sudo easy_install pip
+fi
+
+# Install ansible
+if [[ ! -x `which ansible` ]]; then
+  echo 'Installing ansible...'
+  sudo pip install ansible --quiet
+fi
+
+# Clone short-stack
 if [[ ! -d ~/.short-stack ]]; then
   git clone https://github.com/poetic/short-stack.git ~/.short-stack
 fi
@@ -20,10 +38,12 @@ if [[ ! -d /usr/local/bin ]]; then
   sudo mkdir -p /usr/local/bin
 fi
 
-# Install Short Stack
-if [[ ! -f /usr/local/bin/short-stack ]]; then
-  sudo ln -s ~/.short-stack/short-stack /usr/local/bin/
+# Install short-stack
+if [[ ! -L /usr/local/bin/short-stack ]]; then
+  sudo ln -s ~/.short-stack/short-stack /usr/local/bin/short-stack
 fi
 
-# Run Short Stack
-short-stack
+# Run short-stack unless skip flag present
+if [[ $1 != "--skip-short-stack" ]]; then
+  /usr/local/bin/short-stack
+fi
