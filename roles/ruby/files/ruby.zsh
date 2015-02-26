538f27fa46197522c7a3416e7997ef6741e6b861
Daniel Ochoa <daniel@poeticsystems.com>
2015-02-26 10:01:14 -0600

    move ruby related aliases to ruby/files/ruby.zsh

--- roles/ruby/files/ruby.zsh
+++ roles/ruby/files/ruby.zsh
@@ -1,1 +1,17 @@
+# -------------------------------------------------------------------
+# Ruby Config
+# -------------------------------------------------------------------
+
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
+# -------------------------------------------------------------------
+# Ruby Aliases
+# -------------------------------------------------------------------
+alias rake="noglob rake" # necessary to make rake work inside of zsh
+alias be='bundle exec'
+alias rials='rails'
+alias raisl='rails'
+alias rs='rails s'
+alias rc='rails c'
+alias rdb='rake db:migrate db:test:prepare'
+alias rv='rbenv'
