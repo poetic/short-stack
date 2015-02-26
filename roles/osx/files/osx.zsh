1e58bdd8241e31fe422078fde8e2b36cb4035b61
Daniel Ochoa <daniel@poeticsystems.com>
2015-02-26 10:09:48 -0600

    split the aliases file into its own aliases nested under each task zsh file.

---
+++ roles/osx/files/osx.zsh
@@ -0,0 +1,6 @@
+# -------------------------------------------------------------------
+# Mac only Aliases
+# -------------------------------------------------------------------
+alias ql='qlmanage -p 2>/dev/null' # OS X Quick Look
+alias oo='open .' # open current directory in OS X Finder
+alias find_port='lsof -i ' # what is running on port, find_port :3000
